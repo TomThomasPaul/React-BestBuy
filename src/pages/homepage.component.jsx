5c6afad71c3e528c462341448f785304cfd37f25
TomThomasPaul <jerry.tomomatic@gmail.com>
2020-11-14 15:34:31 +0530

    Built the home page layout with hovering effects and images

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../components/directory/directory.component.jsx';
+
+const Homepage = ()=>{
+
+return (
+  <div className="homepage">
+   <Directory></Directory>
+  </div>
+);
+
+
+}
+
+export default Homepage;
