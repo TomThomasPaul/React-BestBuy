8f6d36ae004232bf091adb17e78296714e1d14c0
TomThomasPaul <jerry.tomomatic@gmail.com>
2020-11-15 18:35:52 +0530

    built the shopping page with collection items containing images,name and price

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
-import Homepage from './pages/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 import {Route, Switch,Link} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
-const HatsPage =(props)=>{
+// const HatsPage =(props)=>{
 
-return <div>
-   {/* <Link to='/'>Home page</Link> */}
+// return <div>
+//    {/* <Link to='/'>Home page</Link> */}
  
-   <button onClick={()=>props.history.push('/')}>Home Page</button>
+//    <button onClick={()=>props.history.push('/')}>Home Page</button>
    
-  <h1>HATS PAGE</h1>
-</div>
+//   <h1>HATS PAGE</h1>
+// </div>
 
 
-}
+// }
 
 function App() {
   return (
@@ -27,7 +28,7 @@ function App() {
      </Switch> */}
 
      <Route exact path='/' component ={Homepage}></Route>
-     <Route  path='/shop/hats' component ={HatsPage}></Route>
+     <Route  path='/shop' component ={ShopPage}></Route>
      
     </div>
   );
