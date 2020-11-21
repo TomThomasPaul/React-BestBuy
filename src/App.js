871a0974709ca69416e702331a4dcf883f6cdb9a
TomThomasPaul <jerry.tomomatic@gmail.com>
2020-11-21 14:49:23 +0530

    built signin and custom button and form input components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Route, Switch,Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from '../src/components/header/header.component';
 
 
@@ -31,6 +32,7 @@ function App() {
      <Switch>
      <Route exact path='/' component ={Homepage}></Route>
      <Route  path='/shop' component ={ShopPage}></Route>
+     <Route  path='/signIn' component ={SignInAndSignUpPage}></Route>
      </Switch>
    
      
