a10698addb0c901fe8511a72de3acc5be3e7b500
TomThomasPaul <70032157+TomThomasPaul@users.noreply.github.com>
2021-01-26 20:31:56 -0500

    Removed logger and added a condition to apply logger only if env is development

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import logger from 'redux-logger'; //middleware example
 
 import combineReducers from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV==="development"){
+    middlewares.push(logger);
+}
 
 export const store = createStore(combineReducers, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
